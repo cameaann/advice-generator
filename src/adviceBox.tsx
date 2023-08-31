63f59c63fec5d10309546c3c2c5f9b9fda8bf763
Anna <cameaann@gmail.com>
2023-08-31 23:17:35 +0300

    added styles and fonts

---
+++ src/adviceBox.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function AdviceBox() {
+  return (
+    <div className="advice">
+      <h1 className="advice-header">Advice # 1</h1>
+      <div className="advice-text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+        debitis, ex molestias deserunt cumque provident corrupti consequuntur
+        vel rem earum cupiditate dolorum inventore, nemo eos, atque dolorem
+        reiciendis enim repudiandae.
+      </div>
+      <div className="advice-decoration"></div>
+    </div>
+  );
+}
+
+export default AdviceBox;
