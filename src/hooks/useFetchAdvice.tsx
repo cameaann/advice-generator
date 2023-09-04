ed6dbbc339d79cbd6d0894da288a2ea99832aaf3
Anna <cameaann@gmail.com>
2023-09-04 19:42:13 +0300

    set advice in hook

--- src/hooks/useFetchAdvice.tsx
+++ src/hooks/useFetchAdvice.tsx
@@ -1,38 +1,34 @@
 import { useEffect, useState } from "react";
 // import { z } from "zod";
 
-
 export function useFetchAdvice() {
+  //   const fetchData = () => {
+  //     return fetch("https://api.adviceslip.com/advice")
+  //         .then((response) => response.json())
+  //         .then((data) => {
+  //             let advice = {
+  //                 id: data.slip.id,
+  //                 text: data.slip.advice
+  //             }
+  //             setAdvice(advice);
+  //         } )
 
-    //   const fetchData = () => {
-    //     return fetch("https://api.adviceslip.com/advice")
-    //         .then((response) => response.json())
-    //         .then((data) => {
-    //             let advice = {
-    //                 id: data.slip.id,
-    //                 text: data.slip.advice
-    //             }
-    //             setAdvice(advice);
-    //         } )
+  //     }
 
-    //     }
+  type Advice = {
+    id: number;
+    text: string;
+  };
 
+  const [advice, setAdvice] = useState<Advice>();
 
-
-    type Advice ={
-        id: number,
-        text: string
-    }
-
-    const [advice, setAdvice] = useState<Advice>({
+  useEffect(() => {
+    // fetchData();
+    setAdvice({
         id: 1,
-        text: "my advice"
-    });
+        text: "my advice",
+      })
+  }, []);
 
-
-//   useEffect(() => {
-//     fetchData();
-//   });
-
-  return [ advice ];
+  return [advice];
 }
