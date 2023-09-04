7602f8c4ad4e05e23297c579f7d55a47dc3dc5af
Anna <cameaann@gmail.com>
2023-09-04 19:44:09 +0300

    fetch data

--- src/hooks/useFetchAdvice.tsx
+++ src/hooks/useFetchAdvice.tsx
@@ -2,19 +2,6 @@ import { useEffect, useState } from "react";
 // import { z } from "zod";
 
 export function useFetchAdvice() {
-  //   const fetchData = () => {
-  //     return fetch("https://api.adviceslip.com/advice")
-  //         .then((response) => response.json())
-  //         .then((data) => {
-  //             let advice = {
-  //                 id: data.slip.id,
-  //                 text: data.slip.advice
-  //             }
-  //             setAdvice(advice);
-  //         } )
-
-  //     }
-
   type Advice = {
     id: number;
     text: string;
@@ -22,12 +9,20 @@ export function useFetchAdvice() {
 
   const [advice, setAdvice] = useState<Advice>();
 
+  const fetchData = () => {
+    return fetch("https://api.adviceslip.com/advice")
+      .then((response) => response.json())
+      .then((data) => {
+        let advice = {
+          id: data.slip.id,
+          text: data.slip.advice,
+        };
+        setAdvice(advice);
+      });
+  };
+
   useEffect(() => {
-    // fetchData();
-    setAdvice({
-        id: 1,
-        text: "my advice",
-      })
+    fetchData();
   }, []);
 
   return [advice];
